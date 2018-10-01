fef8ab99f6caf6ec9b371cbab3d6c900b04c27fa
Henry <cryptix@riseup.net>
2018-10-01 13:49:29 +0200

    implement go-example like example in js

--- go-example/handler.go
+++ go-example/handler.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	"github.com/pkg/errors"
 	"go.cryptoscope.co/muxrpc"
 )
 
 type exampleHandler struct{}
 
-func (h exampleHandler) HandleCall(ctx context.Context, req *muxrpc.Request, _ muxrpc.Endpoint) {
+func (h exampleHandler) HandleCall(ctx context.Context, req *muxrpc.Request, edp muxrpc.Endpoint) {
 	log.Printf("got called: M:%v Args:%v", req.Method, req.Args)
-}
-
-func (h exampleHandler) HandleConnect(ctx context.Context, e muxrpc.Endpoint) {
-	/* calling back
-	ret, err := e.Async(ctx, "str", []string{"whoami"})
-	if err != nil {
-		h.log.Log("handleConnect", "whoami", "err", err)
+	if len(req.Method) != 1 {
+		req.Stream.CloseWithError(errors.New("expected len 1 method"))
 		return
 	}
-	*/
+
+	req.Type = "async" // TOOD: hack - this needs to be moved into muxrpc with the manifest
+
+	switch m := req.Method[0]; m {
+	case "hello":
+		err := req.Return(ctx, fmt.Sprintf("hello %s", req.Args[0]))
+		if err != nil {
+			log.Fatal(errors.Wrap(err, "returning hello failed"))
+		}
+
+	case "callback":
+		type jsonm map[string]interface{}
+		foo, err := edp.Async(ctx, jsonm{}, muxrpc.Method{"callback"}, req.Args)
+		if err != nil {
+			log.Fatal(errors.Wrap(err, "calling back failed"))
+			return
+		}
+		log.Println("got result:", foo)
+		err = req.Return(ctx, foo)
+		if err != nil {
+			log.Fatal(errors.Wrap(err, "returning result failed"))
+		}
+
+	default:
+		req.Stream.CloseWithError(errors.Errorf("undefined method: %s", m))
+	}
 }
+
+func (h exampleHandler) HandleConnect(ctx context.Context, e muxrpc.Endpoint) {}
