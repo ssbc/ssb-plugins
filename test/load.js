1eabfdb5aab75ace69c14dbafe28ebb8d39d6538
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-24 15:49:34 +1300

    reproduce stack overflow bug that @cryptix found

--- test/load.js
+++ test/load.js
@@ -12,7 +12,6 @@ test('load: secret-stack style', (t) => {
     // load our plugin
     const plugPath = join(process.cwd(), 'example')
     let plug = require('../load')(plugPath)
-
     // hand it to the stack and init
     let stack = SecretStack(opt).use(plug)
     let bot = stack(opt)
@@ -24,6 +23,7 @@ test('load: secret-stack style', (t) => {
     bot.hello('alice', (err, greet) => {
         t.error(err)
         t.equal(greet, 'hello alice')
+        console.log('goodbype')
         bot.goodbye((err, val) => {
             t.error(err)
             t.equal(val, 'done')
@@ -37,16 +37,49 @@ test('load: secret-stack style', (t) => {
 test('load#2 - trigger callhost', (t) => {
     const plugPath = join(process.cwd(), 'example')
     let plug = require('../load')(plugPath)
-
+    console.log(plug)
     let stack = SecretStack(opt).use(plug)
     let bot = stack(opt)
 
+    console.log('BOT', bot)
     bot.callhost((err, ret) => {
+        console.log("CALLHOST", err, ret)
         t.error(err)
         var addr = bot.getAddress()
         t.comment('got addr:'+addr)
         t.deepEqual(ret, addr)
+        console.log("goodbye")
         bot.goodbye((err, val) => {
+            t.error(err)
+            t.equal(val, 'done')
+            bot.close(true, () => {
+                t.end()
+            })
+        })
+    })
+})
+
+
+test('load, with name: secret-stack style', (t) => {
+
+    // load our plugin
+    const plugPath = join(process.cwd(), 'example')
+    let plug = require('../load')(plugPath)
+    plug.name = 'example'
+    console.error("PLUG", plug)
+    // hand it to the stack and init
+    let stack = SecretStack(opt).use(plug)
+    let bot = stack(opt)
+
+    for (const fn of ['hello', 'callback', 'crash', 'goodbye']) {
+        t.true(bot.example[fn], `has function ${fn}`)
+    }
+
+    bot.example.hello('alice', (err, greet) => {
+        t.error(err)
+        t.equal(greet, 'hello alice')
+        console.log('goodbype')
+        bot.example.goodbye((err, val) => {
             t.error(err)
             t.equal(val, 'done')
             bot.close(() => {
@@ -55,3 +88,6 @@ test('load#2 - trigger callhost', (t) => {
         })
     })
 })
+
+
+
