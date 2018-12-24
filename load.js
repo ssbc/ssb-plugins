b67112ffc69324295716d1e1fffdffdde5ba28a9
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-24 15:52:26 +1300

    consistent indentation

--- load.js
+++ load.js
@@ -1,18 +1,18 @@
 var path = require('path')
-module.exports = (location) => {
+module.exports = (location, name) => {
   const manifest = require(path.join(location, 'manifest.json'))
   return {
-  // caller has to set name
+  name: name,
   version: 'alpha?',
   manifest: manifest,
   init: (server, conf) => {
-    const localCall = require('muxrpc/local-api')(server, server.getManifest())
-    //prefer not to start child process intil plugin is initialized
-    //otherwise this would fail when testing multiple sbot instances.
-    const {child} = require('./run')(location, localCall)
-    // TODO: how do I pass server to it without creating a loop?
-    return require('muxrpc/api')(server, manifest, child)
-  }
+      const localCall = require('muxrpc/local-api')(server, server.getManifest())
+      //prefer not to start child process intil plugin is initialized
+      //otherwise this would fail when testing multiple sbot instances.
+      const {child} = require('./run')(location, localCall)
+      // TODO: how do I pass server to it without creating a loop?
+      return require('muxrpc/api')({}, manifest, child)
+    }
   }
 }
 
